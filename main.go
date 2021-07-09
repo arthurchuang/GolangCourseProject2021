152a347b775e2898f48ecb19b12a204b5d10ed25
Arthur Chuang <acychuang229@gmail.com>
2021-07-09 11:21:20 +0800

    Add a main.go script to read and print carrefour's HTML body

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	baseURL := "https://www.carrefour.com.tw/"
+	response, err := http.Get(baseURL)
+	if err != nil {
+		log.Fatal("Unable to parse from the baseURL: ", err)
+	}
+	body, errRead := ioutil.ReadAll(response.Body)
+	if errRead != nil {
+		log.Fatal("Failed to read from HTML's body: ", errRead)
+	}
+	fmt.Println(string(body))
+
+	response.Body.Close()
+}
