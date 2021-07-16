e9bf636fe4b9e652d6e59d3e91b8f175b9ebe7f5
Harryu <harry.wang@cienet.com>
2021-07-16 19:06:23 +0800

    briefly discussion

--- readme.m
+++ readme.m
@@ -1,3 +1,3 @@
 The script will return product name, product page link, and product image url
 filter_product_and_price function try to parse data-name to get the product name
-but obvisouly it will return 30 item instead of 24 item, maybe can change to title or something
+but obvisouly it will return 30 item instead of 24 item, maybe the keyword can change to title or other if you want to try
