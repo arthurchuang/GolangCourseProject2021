96bd45b48a38a1127a018dc6bc505460f8eabbba
Johnny Yang <jcyang.dev@gmail.com>
2021-07-18 18:16:34 +0800

    add concurrency with option to customize number of workers

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"GoCrawl/crawl"
+	"context"
 	"flag"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 )
 
@@ -13,6 +18,45 @@ const (
 	storeUrl = "https://online.carrefour.com.tw/tw/"
 	baseUrl  = "https://online.carrefour.com.tw"
 )
+
+type JobPool struct {
+	inputChan  chan string
+	workerChan chan string
+}
+
+func (p JobPool) worker(ctx context.Context, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case url := <-p.workerChan:
+			if err := processPage(url); err != nil {
+				fmt.Printf("Error while processing page (%s) : %e", url, err)
+			}
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func (p JobPool) start(ctx context.Context) {
+	for {
+		select {
+		case url := <-p.inputChan:
+			if ctx.Err() != nil {
+				close(p.workerChan)
+				return
+			}
+			p.workerChan <- url
+		case <-ctx.Done():
+			close(p.workerChan)
+			return
+		}
+	}
+}
+
+func (p JobPool) enqueue(url string) {
+	p.inputChan <- url
+}
 
 func main() {
 	numWorkers := flag.Int("numWorkers", 3, "Use this flag to set the number of workers (default to 3 if not specified).")
@@ -26,19 +70,62 @@ func main() {
 
 	start := time.Now()
 
-	doc.Find(".top1.left-item").Each(func(i int, selection *goquery.Selection) {
-		anchor := selection.Find("a")
-		addr, found := anchor.Attr("href")
-		if found {
-			pageUrl := fmt.Sprintf("%s%s", baseUrl, addr)
-			if err := processPage(pageUrl); err != nil {
-				fmt.Printf("Error while processing page (%s) : %e", pageUrl, err)
-			}
-		}
+	finished := make(chan bool)
+	wg := &sync.WaitGroup{}
+	wg.Add(*numWorkers)
+	jobPool := JobPool{
+		inputChan:  make(chan string, 10),
+		workerChan: make(chan string, *numWorkers),
+	}
+
+	ctx := gracefulShutdown(context.Background(), func() {
+		fmt.Printf("Shutting down gracefully\n")
+		wg.Wait()
+		close(finished)
 	})
 
+	for i := 0; i < *numWorkers; i++ {
+		go jobPool.worker(ctx, wg)
+	}
+
+	go jobPool.start(ctx)
+
+	go func() {
+		doc.Find(".top1.left-item").Each(func(i int, selection *goquery.Selection) {
+			anchor := selection.Find("a")
+			addr, found := anchor.Attr("href")
+			if found {
+				pageUrl := fmt.Sprintf("%s%s", baseUrl, addr)
+				jobPool.enqueue(pageUrl)
+				//if err := processPage(pageUrl); err != nil {
+				//	fmt.Printf("Error while processing page (%s) : %e", pageUrl, err)
+				//}
+			}
+		})
+		close(finished)
+	}()
+
+	<-finished
 	elapsed := time.Since(start)
 	fmt.Printf("\n\nTook %s to process all categories with %d workers.", elapsed, *numWorkers)
+}
+
+func gracefulShutdown(c context.Context, f func()) context.Context {
+	ctx, cancel := context.WithCancel(c)
+	go func() {
+		ch := make(chan os.Signal)
+		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+		defer signal.Stop(ch)
+
+		select {
+		case <-ctx.Done():
+		case <-ch:
+			cancel()
+			f()
+		}
+	}()
+
+	return ctx
 }
 
 func processPage(url string) error {
